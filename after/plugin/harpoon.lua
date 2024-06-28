5061ab49ad90d8ccfa5a90cd8a24ed5fddbb67bc
Brandon Han <bhan48@wisc.edu>
2024-06-28 14:54:04 -0500

    Added harpoon file moving

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -5,3 +5,8 @@ vim.keymap.set("n", "<leader>hl", function() require("harpoon.ui").toggle_quick_
 vim.keymap.set("n", "<leader>hj", function() require("harpoon.ui").nav_next() end)
 vim.keymap.set("n", "<leader>hk", function() require("harpoon.ui").nav_prev() end)
 
+vim.keymap.set("n", "<leader>hh", function() require("harpoon.ui").nav_file(1) end)
+vim.keymap.set("n", "<leader>ht", function() require("harpoon.ui").nav_file(2) end)
+vim.keymap.set("n", "<leader>hn", function() require("harpoon.ui").nav_file(3) end)
+vim.keymap.set("n", "<leader>hs", function() require("harpoon.ui").nav_file(4) end)
+
