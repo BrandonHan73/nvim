d2f4cf7ec300359dffe47ba7aee98c2fee973e6e
Brandon Han <brandontthan2005@gmail.com>
2024-07-12 15:36:42 -0700

    Added which-key plugin

--- lua/apple/lazy.lua
+++ lua/apple/lazy.lua
@@ -51,6 +51,25 @@ plugins = {
 
 	{'tpope/vim-fugitive'},
 
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	}, 
+
 }
 
 require("lazy").setup(plugins)
