05fdf639807868a1f6e75f5377c0e69fa446d980
Brandon Han <brandontthan2005@gmail.com>
2024-07-12 17:51:47 -0700

    Added fugitive commands

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,7 +1,18 @@
 
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
+vim.keymap.set("n", "<leader>gg", vim.cmd.Git);
+
+vim.keymap.set("n", "<leader>gs", function() vim.cmd.Git("status") end);
+vim.keymap.set("n", "<leader>gl", function() vim.cmd.Git("log") end);
+
+vim.keymap.set("n", "<leader>gc", function() vim.cmd.Git("commit") end);
 
 fugitive = {
-	{ "<leader>gs", desc = "Git Status" },
+	{ "<leader>g", group = "Fugitive" },
+	{ "<leader>gg", desc = "Open Fugitive Menu" },
+	{
+		{ "<leader>gs", desc = "Git Status" },
+		{ "<leader>gl", desc = "Git Log" },
+	},
+	{ "<leader>gc", desc = "Git Commit" },
 }
 
