872217edb12e2522a4d1ad24e9501d8ba6898a4f
Brandon Han <brandontthan2005@gmail.com>
2024-10-26 15:33:42 -0500

    Added horizontal split

--- lua/apple/remap.lua
+++ lua/apple/remap.lua
@@ -3,6 +3,7 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>eq", vim.cmd.q, { desc = "Close Buffer" })
 vim.keymap.set("n", "<leader>es", vim.cmd.vs, { desc = "Split Window" })
+vim.keymap.set("n", "<leader>eS", vim.cmd.split, { desc = "Split Window Horizontally" })
 vim.keymap.set("n", "<leader>eh", vim.cmd.nohlsearch, { desc = "Clear Search Highlighting" })
 vim.keymap.set("n", "<leader>ew", vim.cmd.w, { desc = "Save File" })
 vim.keymap.set("n", "<leader>em", vim.cmd.make, { desc = "Make Default Target" })
